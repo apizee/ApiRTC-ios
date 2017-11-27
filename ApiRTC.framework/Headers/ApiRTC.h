8c4ca5bbbab36dbf7feea0e61df4cff1285085d7
Aleksandr Khorobrykh <aleksandr.khorobrykh@gmail.com>
2017-11-27 16:28:40 +0500

    framework

---
+++ ApiRTC.framework/Headers/ApiRTC.h
@@ -0,0 +1,19 @@
+//
+//  ApiRTC.h
+//  ApiRTC
+//
+//  Created by Aleksandr Khorobrykh on 30/10/2017.
+//  Copyright © 2017 Apizee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ApiRTC.
+FOUNDATION_EXPORT double ApiRTCVersionNumber;
+
+//! Project version string for ApiRTC.
+FOUNDATION_EXPORT const unsigned char ApiRTCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApiRTC/PublicHeader.h>
+
+
