1dcc62cb481f0dc527ce3c42ac280a136ff6a8f6
Aleksandr Khorobrykh <aleksandr.khorobrykh@gmail.com>
2017-12-12 18:06:14 +0100

    0.3.1

--- prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-current_pwd="$PWD"
-project_dir=`cd "../../"; pwd`
-cd "$current_pwd"
-project_file=`find "$project_dir" -maxdepth 1 -name "*.xcodeproj" | tail -1`
-
-ruby prepare.rb $project_file
